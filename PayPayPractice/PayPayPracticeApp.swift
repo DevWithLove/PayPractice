77baed4e6965dba42286b5c960969fbf44261f66
Tony Mu <tony.mu@vacasa.com>
2022-08-23 16:24:02 +1200

    Initial Commit

---
+++ PayPayPractice/PayPayPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PayPayPracticeApp.swift
+//  PayPayPractice
+//
+//  Created by Tony Mu on 23/08/22.
+//
+
+import SwiftUI
+
+@main
+struct PayPayPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
