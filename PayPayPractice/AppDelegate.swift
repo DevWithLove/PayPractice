66fdea59d50dfb0470f96aaf9dd7457eab490ee8
Tony Mu <tony.mu@vacasa.com>
2023-08-29 18:04:19 +1000

    Added comment

--- PayPayPractice/AppDelegate.swift
+++ PayPayPractice/AppDelegate.swift
@@ -10,10 +10,17 @@ import AppCenter
 import AppCenterCrashes
 import AppCenterAnalytics
 
+/*
+ Find uuid for dsym
+ mdfind "com_apple_xcode_dsym_uuids == 7323-323-23-3-324234"
+ */
+
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         
         AppCenter.start(withAppSecret: "a9e12a6a-42b5-4d4c-b8d4-8011df266c87", services: [Analytics.self, Crashes.self])
+        
+        print("Had crash: \(Crashes.hasCrashedInLastSession)")
         return true
     }
 }
